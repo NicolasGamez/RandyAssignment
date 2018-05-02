f1f263350132e34729e709d140614c3c6375d076
NicolasGamez <38731864+NicolasGamez@users.noreply.github.com>
2018-05-02 13:49:09 -0500

    LAST WORKING FILE

---
+++ main.py
@@ -0,0 +1,70 @@
+from Weighted_Graph import *
+G = Weighted_Graph("G.txt")
+
+G.draw_graph()
+
+
+#Setting up tree
+V_T = {0}
+E_T= []
+
+T = (V_T,E_T)
+
+
+
+
+
+#Prints cost of edge
+def c(edge):
+    return G.edge_dict()[edge]
+
+print("The cost of edge (1,5) is ", c((1,5)))
+
+
+
+#print("Initial Tree is:", T)
+
+# Find Incident edges
+def incident_edges(T):
+    temp_edges = []
+    for e in G.edge_set():
+        for v in T[0]:
+            if v in e and e not in T[1]:
+                temp_edges.append(e)
+                
+    return temp_edges
+
+'''
+# Find Valid edges
+
+def valid_edges(T):
+    edges = incident_edges(T)
+    temp_edges = []
+    for e in edges:
+        if e[0] not in T[0] or e[1] not in T[0]:
+            temp_edges.append(e)
+    return temp_edges
+
+
+
+print(valid_edges(T))
+print (incident_edges(T))
+'''
+# Find Valid edges
+
+def valid_edges(T):
+    edges = incident_edges(T)
+    temp_edges = []
+    for e in edges:
+        if e[0] not in T[0] and e[1] not in T[0]:
+            temp_edges.append(e)
+    return temp_edges
+
+# Find the minimum of the valid edges
+    
+def min_valid_edges(G,T):
+    edges= min(valid_edges(T))
+    return edges
+
+print(T[1])
+
