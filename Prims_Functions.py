59ac415ffefa955dea0819af1b889ca35dd58330
NicolasGamez <38731864+NicolasGamez@users.noreply.github.com>
2018-05-04 10:50:43 -0500

    Update Prims_Functions.py

--- Prims_Functions.py
+++ Prims_Functions.py
@@ -1,21 +1,14 @@
 from Weighted_Graph import *
-G = Weighted_Graph("graph.txt")
-
+G = Weighted_Graph("G.txt")
+#Setting up tree
 V_T = {0}
 E_T= []
+
 T = (V_T,E_T)
-
-G.draw_graph()
-
-# Prints Cost of edge
 
 def c(edge):
     return G.edge_dict()[edge]
 
-#print("The cost of edge (1,5) is ", c((1,5)))
-#print("Initial Tree is:", T)
-
-# Find Incident edges
 
 def incident_edges(T):
     temp_edges = []
@@ -26,18 +19,42 @@ def incident_edges(T):
                 
     return temp_edges
 
-# Find Valid edges
-
 def valid_edges(T):
     edges = incident_edges(T)
     temp_edges = []
     for e in edges:
-        if e[0] not in T[0] and e[1] not in T[0]:
+        if e[0] not in T[0] or e[1] not in T[0]:
             temp_edges.append(e)
     return temp_edges
 
-# Find the minimum of the valid edges
-    
 def min_valid_edges(G,T):
-    edges= min(valid_edges(T))
-    return edges
+    edges = valid_edges(T)
+    cost = []
+    minimum_edges=[]
+    e=0
+    #print('minimum edges start', minimum_edges )
+    for i in edges:
+        #print(c(edges[e]))
+        cost.append(c(edges[e]))
+        minimum_edges.append(edges[e])
+        e+=1
+    #print(min(cost))
+    #print('minimum edges end', minimum_edges )
+    #print('cost', cost)
+    minimum = min(cost)
+    j=0
+    
+    for x in cost:
+        if minimum == cost[j]:
+            return minimum_edges[j]
+            break
+        j+=1
+        
+    
+    
+'''    for x in cost:
+        if cost[j] == minimum:
+            minimum_edges=[]
+'''
+
+#print(min_valid_edges(G,T))
